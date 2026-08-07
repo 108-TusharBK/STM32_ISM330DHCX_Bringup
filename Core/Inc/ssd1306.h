1a6b1fb5a87c54c93721fd99e37feed111070002
108-TusharBK <k_tushar108@outlook.com>
2026-08-07 22:47:39 +0530

    feat(ssd1306): implement low-level I2C command Interface

---
+++ Core/Inc/ssd1306.h
@@ -0,0 +1,46 @@
+/*
+ * ssd1306.h
+ *
+ *  Created on: Aug 6, 2026
+ *      Author: tushar
+ */
+
+#ifndef INC_SSD1306_H_
+#define INC_SSD1306_H_
+
+#include "main.h"
+#include <stdbool.h>
+
+/**
+ * @brief SSD1306 display resolution.
+ *
+ * 128 × 64 = 8192 pixels
+ * 8192 bits = 1024-byte frame buffer.
+ */
+#define SSD1306_WIDTH      128U
+#define SSD1306_HEIGHT      64U
+
+#define SSD1306_I2C_ADDR  (0x3CU << 1)
+#define SSD1306_BUFFER_SIZE \
+    ((SSD1306_WIDTH * SSD1306_HEIGHT) / 8U)
+
+typedef enum
+{
+    SSD1306_COLOR_BLACK = 0,
+    SSD1306_COLOR_WHITE
+} SSD1306_Color_t;
+
+bool SSD1306_Init(I2C_HandleTypeDef *hi2c);
+
+HAL_StatusTypeDef SSD1306_WriteCommand(uint8_t command);
+
+HAL_StatusTypeDef SSD1306_WriteData(uint8_t *data,
+                                    uint16_t size);
+
+HAL_StatusTypeDef SSD1306_UpdateScreen(uint8_t *data);
+
+HAL_StatusTypeDef SSD1306_DrawPixel(I2C_HandleTypeDef *hi2c);
+
+
+
+#endif /* INC_SSD1306_H_ */
---
+++ Core/Src/ssd1306.c
@@ -0,0 +1,61 @@
+/*
+ * ssd1306.c
+ *
+ *  Created on: Aug 6, 2026
+ *      Author: tushar
+ */
+
+#include "ssd1306.h"
+
+#define SSD1306_CONTROL_COMMAND    0x00
+#define SSD1306_CONTROL_DATA       0x40
+
+static I2C_HandleTypeDef *m_hi2c = NULL;
+
+bool SSD1306_Init(I2C_HandleTypeDef *hi2c)
+{
+	if (hi2c == NULL)
+	{
+		return false;
+	}
+
+	m_hi2c = hi2c;
+
+	return true;
+}
+
+HAL_StatusTypeDef SSD1306_WriteCommand(uint8_t command)
+{
+    if (m_hi2c == NULL)
+    {
+        return HAL_ERROR;
+    }
+
+    return HAL_I2C_Mem_Write(
+        m_hi2c,
+        SSD1306_I2C_ADDR,
+		SSD1306_CONTROL_COMMAND,
+        I2C_MEMADD_SIZE_8BIT,
+        &command,
+        1,
+        HAL_MAX_DELAY);
+}
+
+HAL_StatusTypeDef SSD1306_WriteData(
+    uint8_t *data,
+    uint16_t size)
+{
+	if (m_hi2c == NULL || (data == NULL) || (size == 0U))
+	{
+		return HAL_ERROR;
+	}
+
+	return HAL_I2C_Mem_Write(
+			m_hi2c,
+			SSD1306_I2C_ADDR,
+			SSD1306_CONTROL_DATA,
+			I2C_MEMADD_SIZE_8BIT,
+			&data,
+			1,
+			HAL_MAX_DELAY);
+}
